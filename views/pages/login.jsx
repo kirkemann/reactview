98fbd04e2e1d2594c3ee597fe94cbf1c9def1857
Frank Kirkemann <kirkemann35@gmail.com>
2020-06-11 16:25:19 +0200

    Update login.jsx

--- views/pages/login.jsx
+++ views/pages/login.jsx
@@ -8,15 +8,14 @@ function Login(props) {
 
     <DefaultLayout title={props.sidenstitle}>
     <div>
-    <h1>Login</h1>
+    <h1 className="text-center">Login</h1>
         <form className="text-center" method='post' action='/login'>
           <input className="mx-3" type='email' name='email' placeholder='Email' required />  
           <input className="mx-3" type='password' name='password' placeholder='Password' required />
           <input className="mx-3" type='submit' />  
         </form>
-        <a className="btn btn-danger btn-lg ml-3 my-3" href='/register'>Register</a>
-        <br/>
-        <a className="btn btn-warning btn-lg ml-3 my-3" href='/'>Main</a>
+        <a className="btn btn-danger btn-lg ml-3 my-3 text-center" href='/register'>Register</a>
+        <a className="btn btn-warning btn-lg ml-3 my-3 text-center" href='/'>Main</a>
     </div>
 
 </DefaultLayout>
